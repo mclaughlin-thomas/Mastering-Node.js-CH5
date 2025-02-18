6151eb3b3d49cf0e876d95483f35979757acbfb2
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-18 14:15:12 -0500

    added notes and example headers

--- webapp/dist/handler.js
+++ webapp/dist/handler.js
@@ -9,6 +9,16 @@ exports.handler = void 0;
 // which means that the default types will be used to represent the HTTP 
 // request and response when the handler fcn for the request event is invoked.
 const handler = async (req, resp) => {
+    // Logging request details!
+    console.log(`---- HTTP Method: ${req.method},URL: ${req.url}`);
+    console.log(`host: ${req.headers.host}`);
+    console.log(`accept: ${req.headers.accept}`);
+    console.log(`user-agent: ${req.headers["user-agent"]}`);
     resp.end("Hello, World");
+    // This example writes out the HTTP method, request url, and three headers:
+    // the host header, which specifies the hostname and port to which the request was sent
+    // the accept header which specifies the formats the client is willing to accept in the 
+    // response
+    // and the user-agent header which identifies the client
 };
 exports.handler = handler;
--- webapp/src/handler.ts
+++ webapp/src/handler.ts
@@ -9,5 +9,15 @@ import { IncomingMessage, ServerResponse } from "http";
 // which means that the default types will be used to represent the HTTP 
 // request and response when the handler fcn for the request event is invoked.
 export const handler = async (req: IncomingMessage, resp: ServerResponse) => {
+    // Logging request details!
+    console.log(`---- HTTP Method: ${req.method},URL: ${req.url}`);
+    console.log(`host: ${req.headers.host}`);
+    console.log(`accept: ${req.headers.accept}`);
+    console.log(`user-agent: ${req.headers["user-agent"]}`)
     resp.end("Hello, World");
+    // This example writes out the HTTP method, request url, and three headers:
+    // the host header, which specifies the hostname and port to which the request was sent
+    // the accept header which specifies the formats the client is willing to accept in the 
+    // response
+    // and the user-agent header which identifies the client
 };
