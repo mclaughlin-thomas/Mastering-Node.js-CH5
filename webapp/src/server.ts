025560aa6e7afd2adc479d543f9ee631e1e47b38
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-18 11:00:50 -0500

    same as prev ver just more concise

--- webapp/src/server.ts
+++ webapp/src/server.ts
@@ -30,8 +30,10 @@ import { handler } from "./handler";
 
 const port = 5000;
 const server = createServer();
-server.on("request", handler);
-server.listen(port);
-server.on("listening", () => {
-    console.log(`(Event) Server listening on port ${port}`);
-});
+//server.on("request", handler);
+server.listen(port, () => 
+    console.log(`(Event) Server listening on port ${port}`)
+);
+// server.on("listening", () => {
+    //console.log(`(Event) Server listening on port ${port}`);
+// });
