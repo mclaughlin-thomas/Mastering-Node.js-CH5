a32a642996f40c6878f03abbd4273640ee3d9edb
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-19 09:29:44 -0500

    Understanding HTTP Responses

--- webapp/dist/handler.js
+++ webapp/dist/handler.js
@@ -10,27 +10,26 @@ exports.handler = void 0;
 // request and response when the handler fcn for the request event is invoked.
 const url_1 = require("url");
 const handler = async (req, resp) => {
-    console.log(`---- HTTP Method: ${req.method},URL: ${req.url}`);
-    // console.log(`host: ${req.headers.host}`);
-    // console.log(`accept: ${req.headers.accept}`);
-    // console.log(`user-agent: ${req.headers["user-agent"]}`)
     const parsedURL = new url_1.URL(req.url ?? "", `http://${req.headers.host}`);
-    console.log(`protocol: ${parsedURL.protocol}`);
-    console.log(`hostname: ${parsedURL.hostname}`);
-    console.log(`port: ${parsedURL.port}`);
-    console.log(`pathname: ${parsedURL.pathname}`);
-    parsedURL.searchParams.forEach((val, key) => {
-        console.log(`Search param: ${key}: ${val}`);
-    });
-    resp.end("Hello, World");
-    // try 
-    // http://localhost:5000/myrequest?first=Bob&last=Smith
-    // ---- HTTP Method: GET, URL: /myrequest?first=Bob&last=Smith
-    // protocol: http:
-    // hostname: localhost
-    // port: 5000
-    // pathname: /myrequest
-    // Search param: first: Bob
-    // Search param: last: Smith
+    if (req.method !== "GET" || parsedURL.pathname == "/favicon.ico") {
+        resp.writeHead(404, "Not Found");
+        resp.end();
+        return;
+    }
+    else {
+        resp.writeHead(200, "OK");
+        if (!parsedURL.searchParams.has("keyword")) {
+            resp.write("Hello, HTTP");
+        }
+        else {
+            resp.write(`Hello, ${parsedURL.searchParams.get("keyword")}`);
+        }
+        resp.end();
+        return;
+    }
 };
 exports.handler = handler;
+// This example generates 3 different responses.
+//http://localhost:5000/favicon.ico ,
+//http://localhost:5000?keyword=World , and
+//http://localhost:5000
